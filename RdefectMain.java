ef6ef7784263b9e4cf49c4cae28b00848c298748
Ardi <ardier@outlook.com>
2020-02-04 23:27:47 -0800

    Work in progress for mml file generator

--- RdefectMain.java
+++ RdefectMain.java
@@ -2,52 +2,74 @@
 //The tokens are then saved into a CSV for mutation
 
 
-import java.io.*;
-import java.util.stream.Stream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.*;
-import java.util.NoSuchElementException;
-
 
 
 public class RdefectMain {
 
     long startTime = System.currentTimeMillis();
-        HashMap <String, Integer> tokens = new HashMap<String, Integer>();
+    HashMap<String, Integer> tokens = new HashMap<String, Integer>();
 
 
-       // Throws an exception if the file passed is empty
-       public static void main(String[] args) throws FileNotFoundException,
-       IOException, ClassNotFoundException {
+    // Throws an exception if the file passed is empty
+    public static void main(String[] args) throws
+            IOException, ClassNotFoundException {
 
 
-           //Pass in Tokens
+        //Pass in Tokens
 
-           try {
-               Tokens tokengenerator = new Tokens();
-               //TODO itemize these files in some sort of a list and feed them to the file reader
-               String tokentype="ror";
-               tokengenerator.add(new File("tokens/"+tokentype+".csv"));
-           } catch(Exception e) {
-               //  Block of code to handle errors
-               System.err.println("Token file load error");
-           }
-
-
+        try {
+            Tokens tokengenerator = new Tokens();
+            //TODO itemize these files in some sort of a list and feed them to the file reader
+            String tokentype = "ror";
+            tokengenerator.add(new File("tokens/" + tokentype + ".csv"));
+        } catch (Exception e) {
+            //  Block of code to handle errors
+            System.err.println("Token file load error");
+        }
 
 
+        //TODO: Generate mml file
+        //MML generator. Might be moved to its own class
+        int mmlIndex = 0;
+        String tokenType = "test";
+
+        //1- Add the original tokens to the list
+
+        //2- Add the mutated tokens
+
+        //3- output the mml file
+        List<String> mmlList = Arrays.asList();
+
+        //start of the section
+        mmlList.add("targetOp {");
+
+        //add the mutant operators
+
+        //end of the section
+        mmlList.add("}");
+
+        Path mmlFile = Paths.get("mml/mml" + mmlIndex + ".bin");
+        Files.write(mmlFile, mmlList, StandardCharsets.UTF_8);
 
 
-           if (args.length < 1) {
-               System.err.println("usage: java RdefectMain <filename>");
-               System.exit(1);
-           }
+        if (args.length < 1) {
+            System.err.println("usage: java RdefectMain <filename>");
+            System.exit(1);
+        }
 
 
-        
-           // Printing the first few lines to make sure the file is passed in
+        // Printing the first few lines to make sure the file is passed in
         File file = new File(args[0]); 
   
         BufferedReader br = new BufferedReader(new FileReader(file)); 
@@ -61,7 +83,7 @@ public class RdefectMain {
                 }
             }
 
-
+        //TODO Find or create Java file for tokenization
 
         //TODO mutate the file using the mml file (may have to be done without major)
 
--- Tokens.java
+++ Tokens.java
@@ -1,44 +1,42 @@
 //Given the fact the Major might be able to handle mutation operators
 //this class may not have been necessary, but it was created just in case
 
-import java.util.ArrayList;
-import java.util.*;
-import java.io.*;
+import java.io.File;
+import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Map;
 
-public class Tokens<T> implements TokenInterface<T>{
+public class Tokens<T> implements TokenInterface<T> {
 
     //Delimiters used in the CSV file
     private static final String COMMA_DELIMITER = ";";
     public HashMap<Integer, String> tokensMap;
 
 
-    public Tokens(){
+    public Tokens() {
 
-        tokensMap =  new HashMap<Integer, String>();
+        tokensMap = new HashMap<Integer, String>();
 
     }
 
-    public void add(File Scanner) throws Exception{
+    public void add(File scanner) throws Exception {
 
         //parsing a CSV file into Scanner class constructor
         //TODO enable multiple CSV files
-        Scanner sc = new Scanner((Scanner));
+        Scanner sc = new Scanner((scanner));
         sc.useDelimiter(";");   //sets the delimiter pattern
-        int index=0;
+        int index = 0;
         while (sc.hasNext())  //returns a boolean value
         {
 
-            String tokener=sc.next();
+            String tokener = sc.next();
             System.out.print(tokener);  //find and returns the next complete token from this scanner
             tokensMap.put(index, tokener);
             ++index;
         }
         System.out.println(tokensMap.size());
-        sc.close();  //closes the scanner
+        sc.close();
+        //closes the scanner
 
-        //TODO: Generate mml file
 
     }
 
